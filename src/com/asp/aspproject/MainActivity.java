6a483fead6e4a51dac0bab8c4645bad497bc9889
BenBeni <gohsalhiou@yahoo.fr>
2014-08-13 21:35:17 +0200

    team view on pitch

--- src/com/asp/aspproject/MainActivity.java
+++ src/com/asp/aspproject/MainActivity.java
@@ -1,10 +1,10 @@
 package com.asp.aspproject;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+
 	}
 
 }
